4372174f022f973af8bcb416a3929cff35ec8ca0
Exportens <65998738+Exportens@users.noreply.github.com>
2023-05-22 15:47:12 +0800

    Delete api_gateway directory

--- api_gateway/hertz-gateway/biz/types/response.go
+++
@@ -1,9 +0,0 @@
-package types
-
-const (
-	ResponseErrCode    = "err_code"
-	ResponseErrMessage = "err_message"
-	ResponseNonceStr   = "nonce_str"
-	ResponseSignType   = "sign_type"
-	ResponseSign       = "sign"
-)
